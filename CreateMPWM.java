2b2de15024eb51f576b2ade9910915e2101f7521
MacBook <macbook@APPLEs-MacBook-Pro-2.local>
2012-12-05 06:16:49 +0300

    safter adding some comments

--- CreateMPWM.java
+++ CreateMPWM.java
@@ -3,46 +3,33 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class CreateMPWM {
-        
-    /**
-     * Creates a new instance of <code>CreateMPWM</code>.
-     */
-    public CreateMPWM() {
-    }
-    
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws IOException{
         // TODO code application logic here
         
-        //String fileName = "Gm12878HMM_1_ActivePromoter_10_0_1_1_M.uniqe.ICext";
 	String fileName = args[0];
         Scanner filescan;//to read the file
-		filescan=new Scanner(new File(fileName));//read the whole file
+        filescan=new Scanner(new File(fileName));//read the whole file
 		
-		FileWriter fstream = new FileWriter(fileName.subSequence(0,fileName.indexOf(".uniqe.ICext"))+".uniqe.Mpwm");
+	FileWriter fstream = new FileWriter(fileName.subSequence(0,fileName.indexOf(".uniqe.ICext"))+".uniqe.Mpwm");
         BufferedWriter mpwm = new BufferedWriter(fstream);
 
         String value1, value2, motif, line="";//the readed line as array
 		int index, motifIndex;
 		ArrayList <ArrayList<String>> matrix = new ArrayList <ArrayList<String>>();
-
 		ArrayList<String> row;
 		
 		String [] tokens;
 		line=filescan.nextLine();
 		while (filescan.hasNext())
-	    {
-	       if(line.charAt(0)=='=')
-          {	
+	        {
+	         if(line.charAt(0)=='=')
+                {	
 	        	
 	    	matrix = new ArrayList <ArrayList<String>>();
 			line=filescan.nextLine();
 			motifIndex=line.indexOf("Motif");
 		    motif=line.substring(motifIndex+6, line.indexOf(":"));
 			mpwm.write(">motif"+motif+".dmf"+"\n");
-			
 			
 			while (filescan.hasNext()&&line.charAt(0)!='=')
 			{
