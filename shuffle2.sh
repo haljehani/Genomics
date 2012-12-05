616cea460d461549fbf75740bc9947233b287994
MacBook <macbook@APPLEs-MacBook-Pro-2.local>
2012-12-05 06:17:36 +0300

    after removing the inner loop

--- shuffle2.sh
+++ shuffle2.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 for cell in Gm12878HMM 
 do
-	for state in 5_StrongEnhancer 6_WeakEnhancer  7_WeakEnhancer  8_Insulator  9_TxnTransition 10_TxnElongation 11_WeakTxn 12_Repressed 13_Heterochrom_lo 14_Repetitive_CNV 15_Repetitive_CNV
-	do
-		shuffle -o ./Data/${cell}/${state}.${cell}.bg.fasta ./Data/${cell}/${state}.${cell}.fasta
-	done
+		shuffle -o ./${cell}.bg.fasta ./${cell}.fasta
 done
